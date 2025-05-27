ef2bc033dc0cdfc11b68c247c0b254b5d2bd039c
Khadichabegim <khadichabegim@yandex.com>
2025-05-27 12:43:35 +0500

    test 1.14 v

--- widgets/sso-token-button/dist/runtime/builder-support.js
+++ widgets/sso-token-button/dist/runtime/builder-support.js
@@ -1,20 +1,22 @@
-System.register(["jimu-core","jimu-theme","jimu-ui","jimu-for-builder"],(function(e,t){var s={},i={},o={},n={};return{setters:[function(e){s.ReactRedux=e.ReactRedux,s.classNames=e.classNames,s.css=e.css,s.hooks=e.hooks,s.jsx=e.jsx,s.polished=e.polished},function(e){i.ThemeSwitchComponent=e.ThemeSwitchComponent,i.useTheme=e.useTheme,i.useTheme2=e.useTheme2,i.useUseTheme2=e.useUseTheme2},function(e){o.Link=e.Link,o.defaultMessages=e.defaultMessages},function(e){n.getAppConfigAction=e.getAppConfigAction}],execute:function(){e((()=>{var e={244:e=>{"use strict";e.exports=s},108:e=>{"use strict";e.exports=n},888:e=>{"use strict";e.exports=i},321:e=>{"use strict";e.exports=o}},t={};function u(s){var i=t[s];if(void 0!==i)return i.exports;var o=t[s]={exports:{}};return e[s](o,o.exports,u),o.exports}u.d=(e,t)=>{for(var s in t)u.o(t,s)&&!u.o(e,s)&&Object.defineProperty(e,s,{enumerable:!0,get:t[s]})},u.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),u.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},u.p="";var r={};return u.p=window.jimuConfig.baseUrl,(()=>{"use strict";u.r(r),u.d(r,{default:()=>l});var e=u(244),t=u(888),s=u(321),i=u(108);const o=["default","primary","secondary","tertiary","danger","link"],n=(t,s)=>e.css`
-    width: 360px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: ${t.sys.spacing[3]};
-    padding: ${t.sys.spacing[5]};
-    .button-item{
-      width: 100%;
-      font-size: ${e.polished.rem(13)};
-    }
-    .quick-style-item{
-      padding: ${t.sys.spacing[2]};
-      margin: 2px;
-      &.quick-style-item-selected{
-        outline: 2px solid ${s.sys.color.primary.light};
+System.register(["jimu-core","jimu-theme","jimu-ui","jimu-ui/advanced/setting-components"],(function(e,t){var s={},i={},r={},o={};return{setters:[function(e){s.React=e.React,s.classNames=e.classNames,s.css=e.css,s.jsx=e.jsx,s.polished=e.polished},function(e){i.getBuilderThemeVariables=e.getBuilderThemeVariables},function(e){r.Link=e.Link},function(e){o.QuickStylePopper=e.QuickStylePopper}],execute:function(){e((()=>{"use strict";var e={891:e=>{e.exports=s},796:e=>{e.exports=i},726:e=>{e.exports=r},756:e=>{e.exports=o}},t={};function n(s){var i=t[s];if(void 0!==i)return i.exports;var r=t[s]={exports:{}};return e[s](r,r.exports,n),r.exports}n.d=(e,t)=>{for(var s in t)n.o(t,s)&&!n.o(e,s)&&Object.defineProperty(e,s,{enumerable:!0,get:t[s]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var a={};return(()=>{n.r(a),n.d(a,{default:()=>l});var e=n(891),t=n(796),s=n(726),i=n(756);const r={_widgetLabel:"Button"};class o extends e.React.PureComponent{constructor(t){super(t),this.THEMETYPES=["default","primary","secondary","tertiary","danger","link"],this.translate=e=>{const{intl:t}=this.props;return t?t.formatMessage({id:e,defaultMessage:r[e]}):""},this.getStyle=(t,s)=>e.css`
+      ${s&&`width: ${e.polished.rem(360)};\n        background-color: ${t.colors.palette.light[300]};\n        color: ${t.colors.dark};\n        border: 1px solid ${t.colors.palette.light[500]};\n        box-shadow: 0 4px 20px 4px ${e.polished.rgba(t.colors.white,.5)};`}
+      .button-item{
+        width: 100%;
+        font-size: ${e.polished.rem(13)};
       }
-      background-color: ${t.sys.color.surface.background};
-        cursor: pointer;
-    }
-  `,l={QuickStyle:u=>{var r,l,a;const{widgetId:c}=u,d=e.ReactRedux.useSelector((e=>{var t;return null===(t=(e.appStateInBuilder?e.appStateInBuilder:e).appConfig.widgets[c])||void 0===t?void 0:t.config})),m=!(null===(r=null==d?void 0:d.styleConfig)||void 0===r?void 0:r.useCustom)&&(null===(a=null===(l=null==d?void 0:d.styleConfig)||void 0===l?void 0:l.themeStyle)||void 0===a?void 0:a.quickStyleType)||"default",p=(0,t.useTheme)(),g=(0,t.useTheme2)(),f=(0,t.useUseTheme2)(),y=window.jimuConfig.isBuilder!==f?g:p,h=window.jimuConfig.isBuilder!==f?p:g,v=e.hooks.useTranslation(s.defaultMessages);return(0,e.jsx)("div",{css:n(y,h)},(0,e.jsx)(t.ThemeSwitchComponent,{useTheme2:window.jimuConfig.isBuilder},o.map(((t,o)=>(0,e.jsx)("div",{key:o,className:(0,e.classNames)("quick-style-item",{"quick-style-item-selected":m===t}),onClick:()=>{(e=>{let t=d.setIn(["styleConfig","useCustom"],!1);t=t.setIn(["styleConfig","themeStyle","quickStyleType"],e),t=t.set("styleConfig",t.styleConfig.without("customStyle")),(0,i.getAppConfigAction)().editWidgetConfig(c,t).exec()})(t)}},(0,e.jsx)(s.Link,{title:v("variableButton"),role:"button",className:"button-item text-truncate",type:t},v("variableButton")))))))}}})(),r})())}}}));
+      .quick-style-item-container{
+        padding-left: 4px;
+        padding-right: 4px;
+        padding-bottom: 8px;
+      }
+      .quick-style-item{
+        border: 2px solid transparent;
+        &.quick-style-item-selected{
+          border: 2px solid ${t.colors.palette.primary[600]};
+        }
+        .quick-style-item-inner{
+          ${s&&`background-color: ${t.colors.palette.light[400]};`}
+          cursor: pointer;
+        }
+      }
+    `,this.state={}}render(){const{usePopper:r}=this.props;return r?(0,e.jsx)(i.QuickStylePopper,{reference:this.props.reference,open:!0,placement:"right-start",css:this.getStyle(this.props.theme||(0,t.getBuilderThemeVariables)(),r),onClose:this.props.onClose,onInitDragHandler:this.props.onInitDragHandler,onInitResizeHandler:this.props.onInitResizeHandler,trapFocus:!1,autoFocus:!1},(0,e.jsx)("div",{className:"container-fluid mb-2"},(0,e.jsx)("div",{className:"row no-gutters"},this.THEMETYPES.map(((t,i)=>(0,e.jsx)("div",{key:i,className:"col-4 quick-style-item-container"},(0,e.jsx)("div",{className:(0,e.classNames)("quick-style-item",{"quick-style-item-selected":this.props.selectedType===t})},(0,e.jsx)("div",{className:"quick-style-item-inner p-2",onClick:()=>{this.props.onChange(t)}},(0,e.jsx)(s.Link,{title:this.translate("_widgetLabel"),role:"button",className:"d-inline-block button-item text-truncate",type:t},this.translate("_widgetLabel")))))))))):(0,e.jsx)("div",{className:"container-fluid mb-2",css:this.getStyle(this.props.theme||(0,t.getBuilderThemeVariables)(),r)},(0,e.jsx)("div",{className:"row no-gutters"},this.THEMETYPES.map(((t,i)=>(0,e.jsx)("div",{key:i,className:"col-4 quick-style-item-container"},(0,e.jsx)("div",{className:(0,e.classNames)("quick-style-item",{"quick-style-item-selected":this.props.selectedType===t})},(0,e.jsx)("div",{className:"quick-style-item-inner p-2",onClick:()=>{this.props.onChange(t)}},(0,e.jsx)(s.Link,{title:this.translate("variableButton"),role:"button",className:"d-inline-block button-item text-truncate",type:t},this.translate("variableButton")))))))))}}const l={QuickStyle:o}})(),a})())}}}));
